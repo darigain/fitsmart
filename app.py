00c41cf7f917f9e27058d5831866be55cae1a802
Aidar Gain <67168957+darigain@users.noreply.github.com>
2025-02-11 19:23:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import streamlit as st
-from st_pages import Page, show_pages
+# from st_pages import Page, show_pages
 
-show_pages(
-    [
-        Page("app.py", "🏠 Home"),
-        Page("pages/upload.py", "📤 Upload & Analyze"),
-        # Page("pages/history.py", "📜 Workout History"),
-        Page("pages/statistics.py", "📊 Statistics"),
-        Page("pages/leaderboard.py", "🏆 Leaderboard"),
-        # Page("pages/about.py", "ℹ️ About"),
-    ]
-)
+pg = st.navigation([
+    st.Page("app.py", title="Home", icon="🏠"),
+    st.Page("pages/upload.py", title="Upload & Analyze", icon="📤"),
+    st.Page("pages/statistics.py", title="Statistics", icon="📊"),
+    st.Page("pages/leaderboard.py", title="Leaderboard", icon="🏆"),
+])
+pg.run()
+
+# show_pages(
+#     [
+#         Page("app.py", "🏠 Home"),
+#         Page("pages/upload.py", "📤 Upload & Analyze"),
+#         # Page("pages/history.py", "📜 Workout History"),
+#         Page("pages/statistics.py", "📊 Statistics"),
+#         Page("pages/leaderboard.py", "🏆 Leaderboard"),
+#         # Page("pages/about.py", "ℹ️ About"),
+#     ]
+# )
 st.set_page_config(page_title="FitSmart", page_icon="🏋️")
 
 # GitHub raw URL of your logo (replace with your actual repo URL)
@@ -21,14 +29,14 @@ LOGO_URL = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{REPO_NAME}/mai
 # Display the logo at the top of the page
 st.image(LOGO_URL, width=250)
 
-# Sidebar Navigation
-st.sidebar.title("📍 Navigation")
-st.sidebar.page_link("app.py", label="🏠 Home")
-st.sidebar.page_link("pages/upload.py", label="📤 Upload & Analyze")
-# st.sidebar.page_link("pages/history.py", label="📜 Workout History")
-st.sidebar.page_link("pages/statistics.py", label="📊 Statistics")
-st.sidebar.page_link("pages/leaderboard.py", label="🏅 Leaderboard")
-# st.sidebar.page_link("pages/about.py", label="ℹ️ About")
+# # Sidebar Navigation
+# st.sidebar.title("📍 Navigation")
+# st.sidebar.page_link("app.py", label="🏠 Home")
+# st.sidebar.page_link("pages/upload.py", label="📤 Upload & Analyze")
+# # st.sidebar.page_link("pages/history.py", label="📜 Workout History")
+# st.sidebar.page_link("pages/statistics.py", label="📊 Statistics")
+# st.sidebar.page_link("pages/leaderboard.py", label="🏅 Leaderboard")
+# # st.sidebar.page_link("pages/about.py", label="ℹ️ About")
 
 st.markdown("""
 ## Welcome to FitSmart! 🎉
